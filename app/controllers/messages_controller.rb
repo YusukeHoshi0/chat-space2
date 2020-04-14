a636cd8dc2f2d65dc5dcc67f311682f3294856fe
YusukeHoshin0 <h1133082@gmail.com>
2020-04-14 17:19:40 +0900

    インクリメンタルサーチの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @member = @group.users.map{|u| u.name}.join(", ")
   end
 
   def create
